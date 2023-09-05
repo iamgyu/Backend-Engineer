483fba0f8bc1531f226da4a09eb912b018b876ab
iamgyu <iamgyu@naver.com>
2023-09-05 17:42:18 +0900

    save To-do List to Sqlite

--- TodoList/src/Main.java
+++ TodoList/src/Main.java
@@ -1,44 +1,68 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num;    // 숫자를 입력 받기 위한 변수
+    // SQLite 데이터베이스 연결 정보
+    public static final String DB_URL = "jdbc:sqlite:todo.db";
 
-        Map<String, Todo> todo = TodoFunction.load();
-        if(todo == null){
-            todo = new HashMap<>();
+    public static void main(String[] args) {
+        // SQLite JDBC 드라이를 로드
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC 드라이버를 찾을 수 없습니다.");
+            e.printStackTrace();
+            return;
         }
 
-        while (true) {
-            System.out.println("To-do List");
-            System.out.println("1. 할 일 추가");
-            System.out.println("2. 할 일 목록 조회");
-            System.out.println("3. 할 일 완료");
-            System.out.println("4. 할 일 삭제");
-            System.out.println("그 외 숫자 입력은 종료");
-            System.out.print("숫자를 입력하세요 : ");
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            // 테이블 생성
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS todos (" +
+                    "id TEXT PRIMARY KEY, " +
+                    "title TEXT, " +
+                    "detail TEXT, " +
+                    "done BOOLEAN)");
 
-            try {
-                num = sc.nextInt();
-                String temp = sc.nextLine();    // 오류 해결용 nextLine 추가
-                if (num == 1) {
-                    TodoFunction.input(todo);
-                } else if (num == 2) {
-                    TodoFunction.inquire(todo);
-                } else if (num == 3) {
-                    TodoFunction.done(todo);
-                } else if (num == 4) {
-                    TodoFunction.remove(todo);
-                } else {
-                    System.out.println("그 외 숫자가 입력되었습니다 프로그램을 종료합니다");
+            Scanner sc = new Scanner(System.in);
+            int num;    // 숫자를 입력 받기 위한 변수
+
+            while (true) {
+                System.out.println("To-do List");
+                System.out.println("1. 할 일 추가");
+                System.out.println("2. 할 일 목록 조회");
+                System.out.println("3. 할 일 완료");
+                System.out.println("4. 할 일 삭제");
+                System.out.println("그 외 숫자 입력은 종료");
+                System.out.print("숫자를 입력하세요 : ");
+
+                try {
+                    num = sc.nextInt();
+                    String temp = sc.nextLine();    // 오류 해결용 nextLine 추가
+                    if (num == 1) {
+                        TodoFunction.input(conn);
+                    } else if (num == 2) {
+                        TodoFunction.inquire(conn);
+                    } else if (num == 3) {
+                        TodoFunction.done(conn);
+                    } else if (num == 4) {
+                        TodoFunction.remove(conn);
+                    } else {
+                        System.out.println("그 외 숫자가 입력되었습니다 프로그램을 종료합니다");
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("정수가 아닙니다 프로그램을 종료합니다");
                     break;
                 }
-            } catch (Exception e) {
-                System.out.println("정수가 아닙니다 프로그램을 종료합니다");
-                break;
+                System.out.println();
             }
-            System.out.println();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
